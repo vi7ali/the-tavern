6d44b9a4a546c0fc62556696f106a416462d2740
vi7ali <vi7ali@gmail.com>
2020-05-22 22:10:38 +0300

    Add some js modlues

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('Hello');
+'use strict'
+
+import pageLoad from './page-load';
+import menu from './menu';
+import contact from './contact';
+
+const content = document.getElementById('content');
+const homeBtn = document.getElementById('home-btn');
+const menuBtn = document.getElementById('menu-btn');
+const contactBtn = document.getElementById('contact-btn');
+
+function clearContent() {
+  const buttons = document.getElementsByClassName('nav__button');
+  content.innerHTML = '';
+  for (let btn of buttons)
+    btn.classList.remove('nav__button--active');
+}
+
+function goHome() {
+  clearContent();
+  homeBtn.classList.add('nav__button--active');
+  content.innerHTML = pageLoad();
+}
+
+function goMenu() {
+  clearContent();
+  menuBtn.classList.add('nav__button--active');
+  content.innerHTML = menu();
+}
+
+function goContact() {
+  clearContent();
+  contactBtn.classList.add('nav__button--active');
+  content.innerHTML = contact();
+}
+
+content.innerHTML = pageLoad();
+
+homeBtn.addEventListener('click', pageLoad, false);
+menuBtn.addEventListener('click', goMenu, false);
+contactBtn.addEventListener('click', goContact, false);
